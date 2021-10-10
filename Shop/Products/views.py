986bd307a8362b5eaa0eb8fd4b22fb883655b764
gabrielamadalska <gabriela.madalska@gmail.com>
2021-10-10 19:55:25 +0200

    Django query the database and display the results on the page.

--- Shop/Products/views.py
+++ Shop/Products/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Products
+from .models import Products, Category
 
 # Create your views here.
 
 def index(request):
-    my_request = Products.objects.all() #jak select all z sql
-    return HttpResponse(my_request)
+    all_products = Products.objects.all() #jak select all z sql
+    one = Products.objects.get(pk=2)
+    cat = Products.objects.filter(category=1)
+    cat_name = Category.objects.get(id=4)
+    categories = Category.objects.all()
+    null = Products.objects.filter(category__isnull=True)
+    include = Products.objects.filter(description__contains='polish')
+
+
+
+
+
+    return HttpResponse( include )
