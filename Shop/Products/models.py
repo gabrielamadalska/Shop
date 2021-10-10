3b5b2a1358436146b8fd242499aae157594b1e79
gabrielamadalska <gabriela.madalska@gmail.com>
2021-10-10 17:08:12 +0200

    Add category

--- Shop/Products/models.py
+++ Shop/Products/models.py
@@ -13,6 +13,16 @@ class Producer(models.Model):
         verbose_name = "Producer"
         verbose_name_plural = "Producers"
 
+class Category(models.Model):
+    def __str__(self):
+        return self.name
+
+    name = models.CharField(max_length=25)
+
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
 
 class Products(models.Model):
     def __str__(self):
@@ -20,7 +30,7 @@ class Products(models.Model):
 
     # zwraca string gdy wywołamy obiekt produktu
     # wyswietla nazwe na stronie
-
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.CASCADE)
     producer = models.ForeignKey(Producer, on_delete=models.CASCADE, null=True)
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
