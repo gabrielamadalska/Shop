b03dfe9ec8b2af05f5d0563d4b7a37d37d5f7675
gabrielamadalska <gabriela.madalska@gmail.com>
2021-10-10 15:25:53 +0200

    Add producers database

--- Shop/Products/models.py
+++ Shop/Products/models.py
@@ -3,10 +3,28 @@ from django.db import models
 # Create your models here.
 
 class Products(models.Model):
+    def __str__(self):
+        return self.name
+
+    # zwraca string gdy wywołamy obiekt produktu
+    # wyswietla nazwe na stronie
+
+
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=12, decimal_places=2)
-    #zwraca string gdy wywołamy obiekt produktu
-    #wyswietla nazwe na stronie
+    class Meta:
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
+
+class Producer(models.Model):
     def __str__(self):
         return self.name
+
+    name = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+
+    class Meta:
+        verbose_name = "Producer"
+        verbose_name_plural = "Producers"
+
