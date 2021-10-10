f9d885154ea3d6c4db48237380c1a79cb85575c0
gabrielamadalska <gabriela.madalska@gmail.com>
2021-10-10 16:48:02 +0200

    Define producer in Products. Migrate

--- Shop/Products/models.py
+++ Shop/Products/models.py
@@ -2,21 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-class Products(models.Model):
-    def __str__(self):
-        return self.name
-
-    # zwraca string gdy wywołamy obiekt produktu
-    # wyswietla nazwe na stronie
-
-
-    name = models.CharField(max_length=100)
-    description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=12, decimal_places=2)
-    class Meta:
-        verbose_name = "Product"
-        verbose_name_plural = "Products"
-
 class Producer(models.Model):
     def __str__(self):
         return self.name
@@ -27,4 +12,22 @@ class Producer(models.Model):
     class Meta:
         verbose_name = "Producer"
         verbose_name_plural = "Producers"
+
+
+class Products(models.Model):
+    def __str__(self):
+        return self.name
+
+    # zwraca string gdy wywołamy obiekt produktu
+    # wyswietla nazwe na stronie
+
+    producer = models.ForeignKey(Producer, on_delete=models.CASCADE, null=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=12, decimal_places=2)
+    class Meta:
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
+
+
 
