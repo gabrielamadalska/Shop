66641abdee0457d05d28b2f2ee50ae4eea8459fd
gabrielamadalska <gabriela.madalska@gmail.com>
2021-10-23 20:07:46 +0200

    Working categories - product links.

--- Shop/Products/views.py
+++ Shop/Products/views.py
@@ -16,9 +16,13 @@ def index(request):
     return render(request, 'szablon.html', data)
 
 def category(request, id):
-
     category_user = Category.objects.get(pk=id)
-    return HttpResponse(category_user.name)
+    category_product = Products.objects.filter(category = category_user)
+    categories = Category.objects.all()
+    data = {'category_user' : category_user,
+            'category_product' : category_product,
+            'categories': categories}
+    return render(request, 'category_product.html', data)
 
 def product(request, id):
     product_user = Products.objects.get(pk=id)
